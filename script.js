cbcd31a803e397ffe5f37641fabb189173c5f94e
diogomiguelgaspar <diogo.mrgaspar@gmail.com>
2023-08-24 19:08:22 +0100

    2commit

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-// Get the "View Projects" button element
-const viewProjectsButton = document.querySelector('.hero-content .btn');
+document.addEventListener('DOMContentLoaded', function () {
+  const viewProjectsButton = document.getElementById('view-projects');
+  const projectDetails = document.getElementById('project-details');
 
-// Add a mouseover event listener to the button
-viewProjectsButton.addEventListener('mouseover', () => {
-  // Add a CSS class for the animation when hovering over the button
-  viewProjectsButton.classList.add('btn-bounce');
-});
+  async function fetchProjects() {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const projects = await response.json();
+      return projects;
+    } catch (error) {
+      console.error('Error fetching projects:', error);
+      return [];
+    }
+  }
 
-// Add a mouseout event listener to the button
-viewProjectsButton.addEventListener('mouseout', () => {
-  // Remove the animation class when the mouse moves out of the button
-  viewProjectsButton.classList.remove('btn-bounce');
+  function getRandomProject(projects) {
+    if (projects.length === 0) {
+      return null;
+    }
+    const randomIndex = Math.floor(Math.random() * projects.length);
+    return projects[randomIndex];
+  }
+
+  async function displayRandomProject() {
+    const projects = await fetchProjects();
+    const randomProject = getRandomProject(projects);
+
+    if (randomProject) {
+      projectDetails.innerHTML = `
+        <h3>${randomProject.title}</h3>
+        <p>${randomProject.body}</p>
+      `;
+    } else {
+      projectDetails.innerHTML = '<p>No projects available.</p>';
+    }
+  }
+
+  viewProjectsButton.addEventListener('click', displayRandomProject);
 });
